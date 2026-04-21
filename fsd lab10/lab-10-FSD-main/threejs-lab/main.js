aece3f13f302c424cb9818a2ec60f7968fef6dfd
harijalla396 <harijalla396@gmail.com>
2026-04-21 13:32:30 +0530

    Add files via upload

---
+++ fsd lab10/lab-10-FSD-main/threejs-lab/main.js
@@ -0,0 +1,37 @@
+import * as THREE from "https://unpkg.com/three@0.150.1/build/three.module.js";
+
+console.log("JS running...");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 5;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+document.body.appendChild(renderer.domElement);
+
+// Cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Animation
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();
